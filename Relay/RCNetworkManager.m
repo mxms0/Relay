aa0e2be0da8964051b268ae29d1c414a380eb352
Maximus <imaximusx@gmail.com>
2011-12-24 22:59:22 -0500

    Halfway through implementing something... 0.o

---
+++ Relay/RCNetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  RCNetworkManager.m
+//  Relay
+//
+//  Created by Max Shavrick on 12/24/11.
+//  Copyright (c) 2011 American Heritage School. All rights reserved.
+//
+
+#import "RCNetworkManager.h"
+
+@implementation RCNetworkManager
+
+@end
