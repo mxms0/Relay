ebea95fad72f2acd284908d9a9111835bc1d62d5
Maximus-fs <fuck@you.com>
2012-08-09 11:13:51 -0400

    Working on the little +/- buttons. I need to rely on the current state of the cell instead of the method i attempted to use. Will fix this later, can't think straight atm. :<

--- Relay/RCBasicTableViewCell.h
+++ Relay/RCBasicTableViewCell.h
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-@interface RCBasicTableViewCell : UITableViewCell
-
+@interface RCBasicTableViewCell : UITableViewCell {
+    CGFloat transform;
+    int _state;
+}
 @end
--- Relay/main.m
+++ Relay/main.m
@@ -10,46 +10,8 @@
 #import <objc/runtime.h>
 #import <objc/message.h>
 
-@interface RCSpecialClass : NSObject
-+ (id)deleteImage;
-+ (id)insertImage;
-- (void)blank;
-+ (id)nullRet;
-- (void)_setRotated:(BOOL)rotated animated:(BOOL)animated;
-@end
-@implementation RCSpecialClass
-+ (id)deleteImage {
-	return [UIImage imageNamed:@"0_tminusbtn"];
-}
-+ (id)insertImage {
-	return [UIImage imageNamed:@"0_tplusbtn"];
-}
-- (void)blank {}
-- (void)_setRotated:(BOOL)rotated animated:(BOOL)animated { }
-@end
-
-
 int main(int argc, char *argv[]) {
-	@autoreleasepool {
-		Method origm = class_getClassMethod([objc_getClass("UITableViewCellEditControl") class], @selector(_deleteImage));
-		Method newFF = class_getClassMethod([RCSpecialClass class], @selector(deleteImage));
-		method_exchangeImplementations(origm, newFF);
-		Method fmd = class_getClassMethod([objc_getClass("UITableViewCellEditControl") class], @selector(_insertImage));
-		Method lvfg = class_getClassMethod([RCSpecialClass class], @selector(insertImage));
-		method_exchangeImplementations(fmd, lvfg);
-		Method rgvf = class_getClassMethod([objc_getClass("UITableViewCellEditControl") class], @selector(_updateImageView));
-		Method dkv = class_getClassMethod([RCSpecialClass class], @selector(blank));
-		method_exchangeImplementations(rgvf, dkv);
-		Method sc = class_getClassMethod([objc_getClass("UITableViewCellEditControl") class], @selector(_toggleRotate));
-		Method gd = class_getClassMethod([RCSpecialClass class], @selector(blank));
-		method_exchangeImplementations(sc, gd);
-		Method scd = class_getClassMethod([objc_getClass("UITableViewCellEditControl") class], @selector(setRotated:animated:));
-		Method gdd = class_getClassMethod([RCSpecialClass class], @selector(_setRotated:animated:));
-		method_exchangeImplementations(scd, gdd);
-		Method sfscd = class_getClassMethod([objc_getClass("UITableViewCellEditControl") class], @selector(_multiselectColorChanged));
-		Method gdsad = class_getClassMethod([RCSpecialClass class], @selector(blank));
-		method_exchangeImplementations(sfscd, gdsad);
-		
+	@autoreleasepool {		
 	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([RCAppDelegate class]));
 	}
 }
