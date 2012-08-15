47f11eabed281e542b948acced4535059056e79b
Maximus <imaximusx@gmail.com>
2012-08-15 01:55:53 -0400

    Removed the shadow part of the webview. stupid idea. It's now part of RCNavigationBar which makes more sense.

--- Relay/RCNavigationBar.m
+++ Relay/RCNavigationBar.m
@@ -13,9 +13,31 @@
 
 - (id)initWithFrame:(CGRect)frame {
 	if ((self = [super initWithFrame:frame])) {
+		[self.layer setMasksToBounds:NO];
+		[self setClipsToBounds:NO];
+		RCShadowLayer *shadow = [[RCShadowLayer alloc] init];
+		UIImage *shd = [UIImage imageNamed:@"0_r_shadow"];
+		shadow.contents = (id)shd.CGImage;
+		[shadow setFrame:CGRectMake(0, self.frame.size.height+32, self.frame.size.width, 15)];
+		[shadow setOpacity:0.3];
+		[shadow setShouldRasterize:YES];
+		[self.layer addSublayer:shadow];
+		[shadow release];
     }
     return self;
 }
 
+- (void)layoutSubviews {
+	[super layoutSubviews];
+	float y = self.frame.size.height;
+	if (self.frame.size.width == 320.0f) {
+		y += 32;
+	}
+	for (CALayer *ll in [self.layer sublayers]) {
+		if ([ll isKindOfClass:[RCShadowLayer class]]) {
+			[ll setFrame:CGRectMake(0, y, self.frame.size.width, 15)];
+		}
+	}
+}
 
 @end
